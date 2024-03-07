009e0c0bf0d36cebcbc3ab5f47b69277d54b16fb
Lee-Orr <lee-orr@users.noreply.github.com>
2024-03-07 16:43:14 +0000

    setup template

---
+++ adapter/index.ts
@@ -0,0 +1,17 @@
+interface Book {
+    name: string,
+    description: string,
+    price: number,
+    image: string,
+};
+
+async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
+    throw new Error("Todo")
+}
+
+const assignment = "assignment-1";
+
+export default {
+    assignment,
+    listBooks
+};
